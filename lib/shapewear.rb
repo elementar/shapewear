78636dd7ba4b47e285eb6ae45626141ee8fd026c
Fábio D. Batista <fabio.david.batista@gmail.com>
2011-12-31 03:39:10 -0200

    Kickstarting the DSL.

--- lib/shapewear.rb
+++ lib/shapewear.rb
@@ -1,2 +1,13 @@
+require 'nokogiri'
+
 require 'shapewear/version'
 require 'shapewear/dsl'
+
+# defines String.camelize if it is not defined by, e.g. Rails
+unless String.respond_to? :camelize
+  class String
+    def camelize
+      self.split('_').map(&:capitalize).join
+    end
+  end
+end
--- spec/shapewear/dsl_spec.rb
+++ spec/shapewear/dsl_spec.rb
@@ -1,19 +1,48 @@
 require "spec_helper"
 
 describe Shapewear::DSL do
+
+  let(:xmlns) { {'wsdl' => 'http://schemas.xmlsoap.org/wsdl/', 'soap' => 'http://schemas.xmlsoap.org/wsdl/soap/'} }
+
   describe "basic DSL" do
-    it "should describe parameterless, 'hello world' services"
+    it "should describe parameterless, 'hello world' services" do
+      class ParameterlessHelloWorldService
+        include Shapewear::DSL
+
+        def hello_world
+          "hello"
+        end
+      end
+
+      wsdl = ParameterlessHelloWorldService.to_wsdl
+      puts wsdl
+      wsdl_doc = nil
+      expect { wsdl_doc = Nokogiri::XML(wsdl) { |c| c.strict } }.not_to raise_error
+
+      # there should be a definition with the class' name
+      wsdl_def = wsdl_doc.xpath("/wsdl:definitions[@name='ParameterlessHelloWorldService']", xmlns)
+      wsdl_def.should have(1).node
+      wsdl_def.xpath("wsdl:service[@name='ParameterlessHelloWorldService']/wsdl:port[@name='ParameterlessHelloWorldServicePort']/soap:address", xmlns).should have(1).node
+
+      # the message elements for input and output must be there, both with a part named 'body'
+      wsdl_def.xpath("wsdl:message[@name='HelloWorldInput']/wsdl:part[@name='body']", xmlns).should have(1).node
+      wsdl_def.xpath("wsdl:message[@name='HelloWorldOutput']/wsdl:part[@name='body']", xmlns).should have(1).node
+
+      # there must be an operation named 'HelloWorld'
+      wsdl_def.xpath("wsdl:portType/wsdl:operation[@name='HelloWorld']", xmlns).should have(1).node
+      wsdl_def.xpath("wsdl:binding/wsdl:operation[@name='HelloWorld']", xmlns).should have(1).node
+    end
     it "should describe services with basic parameters and return values"
     it "should describe services with array parameters"
   end
-  
+
   describe "complex types DSL" do
     it "should allow definition of complex types using class introspection"
     it "should allow definition of complex types using a builder-like DSL"
     it "should accept complex types as input"
     it "should accept complex types as output"
   end
-  
+
   describe "WSDL customization" do
     it "should allow customization of namespace"
   end
