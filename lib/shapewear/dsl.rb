e27f406416abaed7c93ea4a7d6d79a4d0ccbc121
Fábio D. Batista <fabio.david.batista@gmail.com>
2012-01-03 18:18:07 -0200

    Adds support for documentation on operations, and for complexTypes as parameters.

--- lib/shapewear/dsl.rb
+++ lib/shapewear/dsl.rb
@@ -1,29 +1,27 @@
 require 'builder'
 
-module Shapewear
-  module DSL
-    private
+module Shapewear::DSL
+  private
 
-    def options
-      @options ||= {}
-    end
+  def options
+    @options ||= {}
+  end
 
-    protected
+  protected
 
-    def wsdl_namespace(ns)
-      options[:wsdl_namespace] = ns
-    end
+  def wsdl_namespace(ns)
+    options[:wsdl_namespace] = ns
+  end
 
-    def schema_namespace(ns)
-      options[:schema_namespace] = ns
-    end
+  def schema_namespace(ns)
+    options[:schema_namespace] = ns
+  end
 
-    def endpoint_url(url)
-      options[:endpoint_url] = url
-    end
+  def endpoint_url(url)
+    options[:endpoint_url] = url
+  end
 
-    def operation(name, ops = {})
-      (options[:operations] ||= {})[name] = ops
-    end
+  def operation(name, ops = {})
+    (options[:operations] ||= {})[name] = ops
   end
 end
